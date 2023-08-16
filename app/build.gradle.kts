8403c435e97b3c640d4a3a5dc17313ac2aed75cc
Haartag <90470991+Haartag@users.noreply.github.com>
2023-08-16 19:00:44 +0200

    Gradle update: material3 * -> 1.1.1, implement icons extended.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.material3ui"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.material3ui"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
